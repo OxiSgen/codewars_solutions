c945a49b7d2e22579f30c5202f63c0ca941ecf1f
OxiSgen <60621483+OxiSgen@users.noreply.github.com>
2022-02-11 01:42:17 +0300

    Update 2_Add_Two_Numbers.py

--- 2_Add_Two_Numbers.py
+++ 2_Add_Two_Numbers.py
@@ -7,19 +7,26 @@ class Solution:
     """
     На входе два односвязных списка представляющие собой два числа в обратном порядке (1 -> 2 -> 4  =  421)
     Нужно вернуть односвязный список представляющий их сумму в таком же виде.
+    Гаранитруется, что список представляет собой число без лидирующих нулей.
     """
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        first, sec = "", ""
-        while l1 or l2:
+        first, sec = "", ""  # Создаём две строки для записи в них наших чисел
+        while l1 or l2: # Проходимся по двум спискам и записываем цифры в строки
             if l1:
                 first += str(l1.val)
                 l1 = l1.next
             if l2:
                 sec += str(l2.val)
                 l2 = l2.next
-        res = reversed(str(int(first[::-1]) + int(sec[::-1])))
-        newList = temp = ListNode()
-        for i in res:
-            temp.next = ListNode(i)
-            temp = temp.next
+        res = reversed(str(int(first[::-1]) + int(sec[::-1]))) # Складываем перевёрнутые в нужную сторону числа, переводим их в строку и опять разворачиваем
+        newList = temp = ListNode() # Создаём два указателя на список. С одним работаем (temp), а другой возвращаем (newList.next)
+        for i in res: 
+            temp.next = ListNode(i) 
+            temp = temp.next # Используем указатель temp для заполнения списка.
+            # для строки "708" temp на каждой итерации будет выглядить следуюем образом:
+            # ListNode{val: 7, next: None}
+            # ListNode{val: 0, next: None}
+            # ListNode{val: 8, next: None}
+        # newList    =   ListNode{val: 0, next: ListNode{val: 7, next: ListNode{val: 0, next: ListNode{val: 8, next: None}}}}.
+        # первый элемент служебный, по этому возвращяем .next
         return newList.next
